ce742e8ba21f588d6e11ae21ec54f92b1ab25f62
Aryan-Protein-Vala <aryansharma24112003@gmail.com>
2026-08-11 17:29:32 +0530

    style: replace framer-motion 3D tilt with native card-paper CSS animation on share preview card per user request

--- components/share-card-preview.tsx
+++ components/share-card-preview.tsx
@@ -1,37 +1,10 @@
 'use client'
 
-import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion';
-
 export function ShareCardPreview({ imageUrl, altText }: { imageUrl: string; altText: string }) {
-  const x = useMotionValue(0.5);
-  const y = useMotionValue(0.5);
-  const springConfig = { damping: 20, stiffness: 300 };
-  const mouseX = useSpring(x, springConfig);
-  const mouseY = useSpring(y, springConfig);
-
-  const rotateX = useTransform(mouseY, [0, 1], [4, -4]);
-  const rotateY = useTransform(mouseX, [0, 1], [-4, 4]);
-
-  function handleMouseMove(e: React.MouseEvent<HTMLDivElement>) {
-    const rect = e.currentTarget.getBoundingClientRect();
-    x.set((e.clientX - rect.left) / rect.width);
-    y.set((e.clientY - rect.top) / rect.height);
-  }
-
-  function handleMouseLeave() {
-    x.set(0.5);
-    y.set(0.5);
-  }
-
   return (
     <div className="w-full md:w-1/2 flex justify-center items-center shrink-0">
-      <motion.div 
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
-        style={{ rotateX, rotateY, transformStyle: 'preserve-3d' }}
-        whileHover={{ scale: 1.02 }}
-        transition={{ type: "spring", stiffness: 300, damping: 20 }}
-        className="relative w-full max-w-[280px] sm:max-w-[340px] md:max-w-[380px] lg:max-w-[420px] aspect-[2/3] transform rotate-[4deg] shadow-[12px_16px_0_#101010] rounded-2xl overflow-hidden border-4 border-black shrink-0 mx-auto cursor-pointer"
+      <div 
+        className="card-paper relative w-full max-w-[280px] sm:max-w-[340px] md:max-w-[380px] lg:max-w-[420px] aspect-[2/3] rotate-[4deg] shadow-[12px_16px_0_#101010] rounded-2xl overflow-hidden border-4 border-black shrink-0 mx-auto cursor-pointer"
       >
         <img 
           src={imageUrl} 
@@ -39,7 +12,7 @@ export function ShareCardPreview({ imageUrl, altText }: { imageUrl: string; altT
           className="w-full h-auto block"
           suppressHydrationWarning
         />
-      </motion.div>
+      </div>
     </div>
   );
 }
